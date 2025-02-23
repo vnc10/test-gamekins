98390b762e5f75fa1e9c3c788eb2e5be8a36e23d
Vinicius Petris <viniciusbosa@gmail.com>
2025-02-23 14:00:05 -0300

    add one test

--- src/test/java/br/com/representacaoexternadedados/service/CursoServiceTest.java
+++ src/test/java/br/com/representacaoexternadedados/service/CursoServiceTest.java
@@ -41,4 +41,17 @@ class CursoServiceTest {
         assertNotNull(result);
         verify(cursoRepository, times(1)).save(any(Curso.class));
     }
+
+    @Test
+    void testCreateCursoNew() {
+        CursoDTO cursoDTO = new CursoDTO("curso novo");
+        Curso curso = new Curso(cursoDTO);
+
+        when(cursoRepository.save(any(Curso.class))).thenReturn(curso);
+
+        Curso result = cursoService.createCurso(cursoDTO);
+
+        assertNotNull(result);
+        verify(cursoRepository, times(1)).save(any(Curso.class));
+    }
 }
