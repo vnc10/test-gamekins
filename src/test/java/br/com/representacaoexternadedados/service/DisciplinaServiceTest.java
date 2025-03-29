4c4cf8338ea9817f34d18ec8890f0ab7f44bdcbd
Vinicius Petris <viniciusbosa@gmail.com>
2025-03-29 16:21:24 -0300

    feature: add test container config

--- src/test/java/br/com/representacaoexternadedados/service/DisciplinaServiceTest.java
+++ src/test/java/br/com/representacaoexternadedados/service/DisciplinaServiceTest.java
@@ -1,14 +1,10 @@
 package br.com.representacaoexternadedados.service;
 
 import br.com.representacaoexternadedados.dto.DisciplinaDTO;
-import br.com.representacaoexternadedados.entity.Aluno;
 import br.com.representacaoexternadedados.entity.Curso;
 import br.com.representacaoexternadedados.entity.Disciplina;
-import br.com.representacaoexternadedados.entity.Matricula;
-import br.com.representacaoexternadedados.repository.AlunoRepository;
 import br.com.representacaoexternadedados.repository.CursoRepository;
 import br.com.representacaoexternadedados.repository.DisciplinaRepository;
-import br.com.representacaoexternadedados.repository.MatriculaRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,25 +13,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.server.ResponseStatusException;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-import static org.junit.Assert.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Testcontainers
 class DisciplinaServiceTest {
 
-    @Container
-    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
-            .withDatabaseName("testdb")
-            .withUsername("test")
-            .withPassword("test");
-
+    static {
+        MySQLTestContainer.getInstance();
+    }
 
     @Autowired
     private DisciplinaRepository disciplinaRepository;
@@ -45,14 +31,6 @@ class DisciplinaServiceTest {
 
     @Autowired
     private DisciplinaService disciplinaService;
-
-    static {
-        mysql.start();
-        System.setProperty("spring.datasource.url", mysql.getJdbcUrl());
-        System.setProperty("spring.datasource.username", mysql.getUsername());
-        System.setProperty("spring.datasource.password", mysql.getPassword());
-        System.setProperty("spring.datasource.driver-class-name", "com.mysql.cj.jdbc.Driver");
-    }
 
     @BeforeEach
     void setup() {
